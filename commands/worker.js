482175e34334f3280b7e5a170ce43c6d88568c33
Alun 'Will' Williams <alun@alunw.com>
2022-09-08 16:31:18 +0100

    Refactored worker command to improve error handling
    As part of this refactoring, the Promise handling to await to improve readability.

--- commands/worker.js
+++ commands/worker.js
@@ -4,9 +4,10 @@ const path = require('path');
 const axios = require('axios');
 const chalk = require('chalk');
 const YAML = require('js-yaml');
+const { exit } = require('process');
 
 // foundational HTTP setup to Cloudflare's API
-axios.defaults.baseURL = 'https://api.cloudflare.com/client/v4';
+axios.defaults.baseURL = 'https://api.cloudflare.com/client/v4'; 
 
 /**
  * Describe a redirect as a YAML file
@@ -18,50 +19,62 @@ exports.builder = (yargs) => yargs.demandOption('configDir')
     describe: 'Domain to redirect',
     type: 'string'
   });
-exports.handler = (argv) => {
+exports.handler = async (argv) => {
+  // Check 
   axios.defaults.headers.common.Authorization = `Bearer ${argv.cloudflareToken}`;
 
   const { configDir, domain } = argv;
   // open the redirects file
   const filepath = path.join(configDir.name, `${domain}.yaml`);
   const description = YAML.load(fs.readFileSync(filepath));
-  // push it to the KV
-  // TODO: check (earlier than here!) whether WR_WORKER_KV_NAMESPACE is set
-  axios.put(`/accounts/${argv.accountId}/storage/kv/namespaces/${argv.workerKvNamespace}/values/${domain}`, description)
-    .then(({ data }) => {
+
+  try {
+    let data;
+
+    // Get zone id
+    ({ data } = await axios.get(`/zones?name=${domain}`));
+    if (data.result.length === 0) {
+      console.error('ERROR: No matching domain found!');
+      exit(1);
+    }
+    if (data.result.length > 1) {
+      console.error(`ERROR: Multiple domains found! result = ${data.result}`);
+      exit(1);
+    }
+    let zone_id = data.result[0].id;
+
+    // Store the KV redirect values
+    // TODO: check (earlier than here!) whether WR_WORKER_KV_NAMESPACE is set
+    ({ data } = await axios.put(`/accounts/${argv.accountId}/storage/kv/namespaces/${argv.workerKvNamespace}/values/${domain}`, description));
+    if (data.success) {
+      console.log('Redirect description stored in Key Value storage successfully!');
+    } else {
+      console.error('FAILED to store the redirect description in Key Value storage!');
+      exit(1);
+    }
+
+    // TODO: handle situations where more than just `www` and the apex redirect
+    [domain, `www.${domain}`].forEach(async (hostname) => {
+      // setup the Worker route or Worker custom domain
+      ({data} = await axios.put(`/accounts/${argv.accountId}/workers/domains`, {
+        zone_id,
+        hostname,
+        service: 'redir',
+        environment: 'production'
+      }));
       if (data.success) {
-        console.log('Redirect Description stored in Key Value storage successfully!');
+        console.log(`Setup ${hostname} to point to the ${chalk.bold('redir')} Worker.`);
+      } else {
+        console.error(`FAILED to create custom domain for ${hostname}!`);
       }
-    })
-    .catch(console.error);
-  // get the zone ID for the domain in question
-  axios.get(`/zones?name=${domain}`)
-    .then(({ data }) => {
-      if (data.result.length > 0) {
-        return data.result[0].id;
-      }
-      throw JSON.stringify(data.errors);
-    })
-    .then((zone_id) => {
-      // TODO: handle situations where more than just `www` and the apex redirect
-      [domain, `www.${domain}`].forEach((hostname) => {
-        // setup the Worker route or Worker custom domain
-        axios.put(`/accounts/${argv.accountId}/workers/domains`, {
-          zone_id,
-          hostname,
-          service: 'redir',
-          environment: 'production'
-        }).then(() => {
-          console.log(`Setup ${hostname} to point to the ${chalk.bold('redir')} Worker.`);
-        }).catch((err) => {
-          if (err.response.status === 409) {
-            console.error(`Failed to setup ${hostname} due to conflict.`);
-            console.error(err.response.data.errors);
-          } else {
-            console.error(err);
-          }
-        });
-      });
-    })
-    .catch(console.error);
+    });
+
+  } catch(err) {
+    if (err.response) {
+      console.error(`ERROR: Response = ${err.response.status} ${err.response.statusText}; Request = ${err.response.config.method.toUpperCase()} ${err.response.config.url}`);
+    } else {
+      console.error(err);
+    }
+  }
+
 };
